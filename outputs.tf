1ffcf2c2f56f3fc7d025f1b85465c3b54c4c1833
Matthew Sherborne <msherborne@gmail.com>
2021-06-08 11:56:52 +1000

    Adde security group into the outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "instance_id" {
   value = aws_instance.sysops_study.id
 }
 
+output "security-group-id" {
+  value = aws_security_group.cloud_watch.id
+}
+
 output "instance_dns_name" {
   value = aws_instance.sysops_study.public_dns
 }
